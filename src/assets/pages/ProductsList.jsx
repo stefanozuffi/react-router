be086c5cb00ace3f764e51e898b6f13c179fbb0a
stefanozuffi <stefano.zuffi06@outlook.com>
2025-10-14 21:09:34 +0200

    basic implementation of the product page

--- src/assets/pages/ProductsList.jsx
+++ src/assets/pages/ProductsList.jsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+import ProductCard from "../components/ProductCard"
+
 export default function ProductsList() {
+    const [products, setProducts] = useState([])
+
+    function fetchData(endpoint) {
+        axios.get(endpoint)
+        .then(res => setProducts(res.data))
+    }
+
+    useEffect(() => {
+        fetchData('https://fakestoreapi.com/products')
+    }, [])
+
     return(
         <>
             <header className="container">
                 <h1>Our Products</h1>
             </header>
             <main className="container">
-                <p>we are working on it...</p>
+                <div className="container">
+                    <div className="row">
+                        {products.map(product => 
+                            <ProductCard title={product.title} image={product.image} price={product.price} key={product.id}/>
+                        )}
+                    </div>
+                </div>
+                
             </main>
         </>
         
