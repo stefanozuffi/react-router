e81f26ca8de8b86d9863396fe89a24f916d90141
stefanozuffi <stefano.zuffi06@outlook.com>
2025-10-19 13:06:16 +0200

    cards and header styling with css

--- src/assets/pages/ProductsList.jsx
+++ src/assets/pages/ProductsList.jsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
 import ProductCard from "../components/cardss/ProductCard"
+import { Superballs } from 'ldrs/react'
+import 'ldrs/react/Superballs.css'
 
 
 export default function ProductsList() {
     const [products, setProducts] = useState([])
+
+    setTimeout(() => {
+        fetchData('https://fakestoreapi.com/products')
+    }, 3000)
 
     function fetchData(endpoint) {
         axios.get(endpoint)
@@ -17,11 +23,17 @@ export default function ProductsList() {
 
 
     return(
-            <main className="container">
-                <div className="container">
-                <div className="row g-5">
+            <main className="container"> 
+                {/* {!products &&
+                    <Superballs
+                      size="40"
+                      speed="1.4"
+                      color="black" 
+                    />}  this function will be added for the single product card*/}
+                <div className="container"> 
+                <div className="row g-5"> 
                     {products.map(item => 
-                        <ProductCard title={item.title} image={item.image} price={item.price} description={item.description} key={item.id} rating={item.rating.rate} category={item.category}/>
+                         <ProductCard title={item.title} image={item.image} price={item.price} description={item.description} key={item.id} rating={item.rating.rate} category={item.category}/>
                     )}
                 </div>
             </div>
