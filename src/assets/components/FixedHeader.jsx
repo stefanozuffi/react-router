d156f3960cd1627fd2a462873e49c41ae0876ee2
stefanozuffi <stefano.zuffi06@outlook.com>
2025-10-21 12:47:46 +0200

    added and styled elements to the homepage

--- src/assets/components/FixedHeader.jsx
+++ src/assets/components/FixedHeader.jsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom"
 export default function FixedHeader() {
     return(
         <div className="header d-flex justify-content-around align-items-center p-3">
-                <h4>The Fake E-Commerce</h4>
-                <nav className="d-flex gap-5">
+                <h4>The Fake<br></br> E-Commerce</h4>
+                <nav className="d-flex flex-sm-column flex-md-row gap-3">
                     <NavLink to='/'>Home</NavLink>
                     <NavLink to='/products'>Products</NavLink>
                     <NavLink to='/us'> Contacts</NavLink>
--- src/assets/pages/HomePage.jsx
+++ src/assets/pages/HomePage.jsx
@@ -1,5 +1,10 @@
 import { NavLink } from "react-router-dom";
 import ShoppingPic from '../../img/undraw_web-shopping_xd5k.svg'
+import ShoppingPic2 from '../../img/GreenWebShop.svg'
+import EmptyCart from '../../img/EmptyCart.svg'
+import WishList from '../../img/WishList.svg'
+import FullCartel from '../../img/FullCartel.svg'
+
 
 export default function HomePage() {
     return(
@@ -10,11 +15,11 @@ export default function HomePage() {
             <div className="home-ctn d-flex flex-column gap-3">
 
                  {/* Presentation */}
-                    <div className="home-ctn-in d-flex justify-content-around">
-                        <div className="jumbo home p-5 mb-4 bg-light rounded-3">
-                            <div className="container-fluid py-5">
+                    <div className="home-ctn-in d-flex justify-content-between flex-sm-column flex-md-row align-items-sm-center mb-4">
+                        <div className="jumbo home d-flex flex-column align-items-center">
+                            <div className="container-fluid py-2">
                                 <h2 className="display-5 fw-bold"> Dive Into Inexistent Products! </h2>
-                                <p className="col-md-8 fs-4">
+                                <p className="col-md-11">
                                 Welcome to The Fake E-Commerce, where imagination meets reality! 
                                 Explore our curated collection of products that don't actually exist, 
                                 but look absolutely stunning. From cutting-edge electronics to timeless 
@@ -22,17 +27,33 @@ export default function HomePage() {
                                 portfolios, or just for browsing pleasure. No real transactions, 
                                 just pure shopping experience!
                                 </p>
-                                <NavLink to={'/products'} className="btn btn-primary btn-lg" type="button">
+                                <NavLink to={'/products'} className="j-btn" type="button">
                                     Start Shopping
                                 </NavLink>
                             </div>
                         </div>
                         <div className="pic-ctn">
-                            <img src={ShoppingPic} alt="shopping picture" />
+                            <img src={ShoppingPic2} alt="shopping picture" />
                         </div>
                     </div>
 
                     {/* invent a list of things and hold a cart, if you have one */}
+                    <div className="home-ctn-in d-flex flex-column justify-content-between align-items-sm-center mb-4">
+                        <div className="text jumbo home">
+                                <h2 className="display-5 fw-bold"> Think About Something You Might Need! </h2>
+                                <p className="">
+                                You won't get it, but you can completely simulate how it would be to buy that product online, from ebay to even Amazon!
+                                </p>
+                        </div>
+                        <div className="pic-ctn-lg d-flex justify-content-around">
+                            <img className="big-pic" src={WishList} alt="shopping picture"/>
+                            <div className="small-pics-ctn d-flex flex-column gap-2">
+                                <img className="small-pic ps-5" src={EmptyCart} alt="shopping picture"/>
+                                <img className="small-pic pe-5" src={FullCartel} alt="shopping picture"/>
+                            </div>
+                            
+                        </div>
+                    </div>
             </div>
             
             
